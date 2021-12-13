26c6cb7bfdba2bfd9cf22519774e451d3b23a745
Patrick <plwilkie@gmail.com>
2021-12-13 00:00:18 -0800

    Fix files 1-4

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import asyncio
 import time
 
-for i in range(64,73):
+for i in range(4,5):
     driver = webdriver.Safari()
 
     my_url = "https://complexity.simplecast.com/episodes/"+str(i)+"/transcript"
@@ -21,19 +21,3 @@ for i in range(64,73):
         driver.quit()
     
     time.sleep(2)
-
-
-
-# def main():
-#     loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(loop)
-    
-#     result = loop.run_until_complete(get_episode_text(index))
-    
-
-# async def get_episode_text(index):
-#     # i = index
-#     # my_url = "https://complexity.simplecast.com/episodes/"+str(i)+"/transcript"
-#     await driver.get(my_url)
-#     transcript = driver.find_element_by_class_name("sc-episode-details-body")
-#     return transcript
